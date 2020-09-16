06a78f87b6e69ee05d67b0ef522c39c283a54588
Marcos Vinicius <marcosporto1963@gmail.com>
2020-09-16 23:26:40 +0000

    dicas C

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  printf("Ola Mundo\n");
   return 0;
+
+//texto
+/*PSEUDOCODIGO           |C
+Inteiro                  = int(%d)
+real                     = float(%f) ou double (%lf)
+caracter                 = char(%c)
+cadeia                   =char[10] (%s)
+
+escreva("OK")            =printf("OK");
+leia(dia)                =scanf("%d", &dia);
+      
+Idade ;25 anos
+escreva("Idade :" ,idade, " anos\n")
+printf("Idade ; %d anos\n", idade);
+
+leia(idade,peso)
+scanf("%d%f",&idade ,&peso);
+*/
+
 }
